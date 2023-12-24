8fdf599b0d8cd92eb11b18d26c7cc6a4f0b889b0
kxsong554520 <s65545@ocean.umt.edu.my>
2023-12-24 20:15:22 +0800

    Initial commit

---
+++ app/src/main/java/com/example/saveforplans/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.saveforplans
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onClickAddPlans(view: View) {
+
+    }
+
+    fun onClickViewExistingPlans(view: View) {
+
+    }
+
+    fun onClickBankAccountExpenses(view: View) {
+        
+    }
+}
